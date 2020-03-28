ad6d744980c1a1237727ab70b4ef1f9ffc30b1b5
MatthewSDick <matthew.dick@outlook.com>
2020-03-28 14:49:43 -0400

    Order page complete

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -7,53 +7,92 @@ const OrderPage = () => {
     <div>
       <Header />
       <h1>Order Page</h1>
-      <div
-        className="order-page"
-        style={{ borderStyle: 'solid', borderColor: 'green' }}
-      >
-        <div
-          className="order-page-left"
-          style={{ borderStyle: 'solid', borderColor: 'black' }}
-        >
-          <h1>Left</h1>
+      <div className="order-page">
+        <div className="order-page-left">
+          <div className="cart-view">
+            <h3 className="order-cart-head">Cart</h3>
+            {/* loop cart items  */}
+
+            <div className="order-cart-list-item">
+              <div className="order-cart-image-box">
+                <img
+                  className="order-cart-image"
+                  src="./images/pepperoni-pizz.jpg"
+                />
+              </div>
+              <div className="order-cart-details">
+                <p>Homemade Lasagna</p>
+                <p>
+                  4 X $ <span style={{ color: '#CA0707' }}>14.95</span>
+                </p>
+              </div>
+            </div>
+            {/* another loop */}
+            <div className="order-cart-list-item">
+              <div className="order-cart-image-box">
+                <img
+                  className="order-cart-image"
+                  src="./images/pepperoni-pizz.jpg"
+                />
+              </div>
+              <div className="order-cart-details">
+                <p>Homemade Lasagna</p>
+                <p>
+                  4 X $ <span style={{ color: '#CA0707' }}>14.95</span>
+                </p>
+              </div>
+            </div>
+            <div>
+              <h3 className="order-subtotal">
+                Subtotal: <span style={{ color: '#CA0707' }}>$ 99.99</span>
+              </h3>
+            </div>
+            <div className="order-buttons">
+              <div className="order-buttons-left">
+                <button className="order-left-button">VIEW CART</button>
+              </div>
+              <div className="order-buttons-right">
+                <button className="order-right-button">CHECKOUT</button>
+              </div>
+            </div>
+
+            {/* end items loop */}
+          </div>
         </div>
 
-        <div
-          className="order-page-right"
-          style={{ borderStyle: 'solid', borderColor: 'red' }}
-        >
-          <h1>Right</h1>
+        <div className="order-page-right">
+          {/* <h1>Right</h1> */}
           <ul className="catagory-list">
             {/* map from API */}
             <li className="order-item">
               <img className="order-image" src="./images/pepperoni-pizz.jpg" />
               <p>Item Name</p>
               <p>$ 99.99</p>
-              <button className="add-to-cart">ADD TO CART</button>
+              <button className="order-add-to-cart-btn">ADD TO CART</button>
             </li>
             <li className="order-item">
               <img className="order-image" src="./images/pepperoni-pizz.jpg" />
               <p>Item Name</p>
               <p>$ 99.99</p>
-              <button className="add-to-cart">ADD TO CART</button>
+              <button className="order-add-to-cart-btn">ADD TO CART</button>
             </li>
             <li className="order-item">
               <img className="order-image" src="./images/pepperoni-pizz.jpg" />
               <p>Item Name</p>
               <p>$ 99.99</p>
-              <button className="add-to-cart">ADD TO CART</button>
+              <button className="order-add-to-cart-btn">ADD TO CART</button>
             </li>
             <li className="order-item">
               <img className="order-image" src="./images/pepperoni-pizz.jpg" />
               <p>Item Name</p>
               <p>$ 99.99</p>
-              <button className="add-to-cart">ADD TO CART</button>
+              <button className="order-add-to-cart-btn">ADD TO CART</button>
             </li>
             <li className="order-item">
               <img className="order-image" src="./images/pepperoni-pizz.jpg" />
               <p>Item Name</p>
               <p>$ 99.99</p>
-              <button className="add-to-cart">ADD TO CART</button>
+              <button className="order-add-to-cart-btn">ADD TO CART</button>
             </li>
             {/* bottom of the map loop */}
           </ul>
